df731a324c9ac2943f85ae6a1e19e7db413f360e
caferion <caferion@ex-em.com>
2019-11-28 02:26:53 +0900

    ``

---
+++ new/1124-longestCommonPrefix.js
@@ -0,0 +1,12 @@
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) {
+        return '';
+    }
+    let result = strs[0];
+    for(let ix = 1; ix < strs.length; ix++) {
+        while(strs[ix].indexOf(result) !== 0) {
+            result = result.substring(0, result.length-1)
+        }
+    }
+    return result;
+};
